226d5d879239b74a01145ea8249d0222c4f2ca3c
Петр Халиков <pkhalikov@dataplus.ru>
2024-09-07 20:44:14 +0300

    theme & editor config

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,11 +1,19 @@
+import Aura from '@primevue/themes/aura';
+
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  compatibilityDate: '2024-04-03',
-  devtools: { enabled: true },
-  modules: [
-    '@nuxt/image',
-    '@nuxt/icon',
-    '@primevue/nuxt-module',
-    '@pinia/nuxt',
-  ],
+    compatibilityDate: '2024-04-03',
+    devtools: { enabled: true },
+    modules: [
+        '@nuxt/image',
+        '@nuxt/icon',
+        ['@primevue/nuxt-module', {
+        options: {
+            theme: {
+                preset: Aura,
+            }
+        }
+        }],
+        '@pinia/nuxt',
+    ],
 })
